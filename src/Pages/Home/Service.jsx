46d60d7ad196083b982cad831fbde917a3208fc6
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-03-10 22:43:15 +0600

    heroku service route live link add

--- src/Pages/Home/Service.jsx
+++ src/Pages/Home/Service.jsx
@@ -5,7 +5,7 @@ const Service = () => {
     const [service, setService] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch('https://fierce-thicket-41993.herokuapp.com/service')
             .then(res => res.json())
             .then(data => setService(data))
     }, []);
@@ -14,12 +14,23 @@ const Service = () => {
     return (
         <div>
             <h1 className='text-center text-orangeDip text-2xl font-bold py-3'>My Service</h1>
-
-            <div className="flex justify-between sm:px-2 md:px-5 sm:flex-col md:flex-row ">
-                {
-                    service?.map(service => <ServiceCart service={service} key={service._id}></ServiceCart>)
-                }
-            </div>
+            {
+                service?.length
+                    ?
+                    <div className="flex justify-between sm:px-2 md:px-5 sm:flex-col md:flex-row ">
+                        {
+                            service?.map(service => <ServiceCart service={service} key={service._id}></ServiceCart>)
+                        }
+                    </div>
+                    :
+                    <div className="text-center p-10">
+                        <span class="flex h-3 w-3">
+                            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                            <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                            loading...
+                        </span>
+                    </div>
+            }
         </div>
     );
 };
